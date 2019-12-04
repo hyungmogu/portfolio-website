f45247822650ff8954f7409583b478b7a6dc1825
Hyungmo Gu <guhyungm7@gmail.com>
2019-12-04 01:52:56 -0800

    feat: Add model 'home page'

--- project/main/models.py
+++ project/main/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
-# Create your models here.
+class Project(models.Model):
+    title = models.CharField(max_length=255)
+    summary = models.CharField(max_length=255)
+    url_demo = models.URLField()
+    url_source_code = models.URLField()
+    challenges = models.TextField()
+    result = models.TextField()
+
+    def __str__(self):
+        return self.title
