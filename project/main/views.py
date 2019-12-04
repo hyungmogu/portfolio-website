496aa772128400ae17cf3a9638268224917cc343
Hyungmo Gu <guhyungm7@gmail.com>
2019-12-04 03:06:42 -0800

    fix: correct model not found error 'projects, home page'

--- project/main/views.py
+++ project/main/views.py
@@ -1,14 +1,17 @@
 from django.shortcuts import render
 from django.views.generic import TemplateView
 
+from . import models
+
 # Create your views here.
 
 class HomeView(TemplateView):
+    models=models.Project
     template_name = 'main/projects.html'
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
-        projects = models.projects.objects.all()
+        projects = models.Project.objects.all()
         context['projects'] = projects
 
         return context
