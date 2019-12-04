7059d8aed828b09068bd552798048955b6320ea1
Hyungmo Gu <guhyungm7@gmail.com>
2019-12-04 01:28:48 -0800

    feat: Add basic server logic 'home page'

--- project/main/views.py
+++ project/main/views.py
@@ -6,3 +6,9 @@ from django.views.generic import TemplateView
 class HomeView(TemplateView):
     template_name = 'main/projects.html'
 
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        projects = models.projects.objects.all()
+        context['projects'] = projects
+
+        return context
