cda30034521e71720eddcb6e958318716ee71d86
Hyungmo Gu <guhyungm7@gmail.com>
2019-12-04 19:49:50 -0800

    feat: Add url 'project detail'

--- project/main/urls.py
+++ project/main/urls.py
@@ -2,5 +2,6 @@ import main.views as views
 from django.urls import path, include
 
 urlpatterns = [
-    path('', views.HomeView.as_view(), name="home"),
+    path('', views.HomeView.as_view(), name='home'),
+    path('projects/<int:pk>', views.ProjectView.as_view(), name='project'),
 ]
