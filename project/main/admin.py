08385823c24c892ae5959c8d29837a05ffb74c5b
Hyungmo Gu <guhyungm7@gmail.com>
2019-12-05 19:48:34 -0800

    feat: Add project to admin

--- project/main/admin.py
+++ project/main/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
 
-# Register your models here.
+from . import models
+
+# class TextInline(admin.StackedInline):
+#     model = models.Text
+
+# class QuizInline(admin.StackedInline):
+#     model = models.Quiz
+
+# class CourseAdmin(admin.ModelAdmin):
+#     inlines = [TextInline, QuizInline]
+
+admin.site.register(models.Project) #register by adding this line
