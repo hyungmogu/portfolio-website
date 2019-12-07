460fcd4c7ae2337db4454dda5db59ade683de18b
Hyungmo Gu <guhyungm7@gmail.com>
2019-12-07 12:35:40 -0800

    fix: fix amazon aws eb not working  -  downgraded django to 2.1.1

--- project/project/wsgi.py
+++ project/project/wsgi.py
@@ -4,7 +4,11 @@ WSGI config for project project.
 It exposes the WSGI callable as a module-level variable named ``application``.
 
 For more information on this file, see
+<<<<<<< HEAD
 https://docs.djangoproject.com/en/3.0/howto/deployment/wsgi/
+=======
+https://docs.djangoproject.com/en/2.1/howto/deployment/wsgi/
+>>>>>>> fix: downgrade to django 2.1.1
 """
 
 import os
