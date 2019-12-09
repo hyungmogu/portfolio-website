90c916b91323f71aef5f9cba13df25ce34dd8fb6
Hyungmo Gu <guhyungm7@gmail.com>
2019-12-08 20:17:21 -0800

    feat: Add date created 'project detail'

--- project/main/models.py
+++ project/main/models.py
@@ -1,8 +1,10 @@
 from django.db import models
+from datetime import datetime
 
 class Project(models.Model):
     title = models.CharField(max_length=255)
     summary = models.CharField(max_length=255)
+    date_created = models.DateTimeField(default=datetime.now, blank=True)
     image_main = models.URLField()
     url_demo = models.URLField()
     url_source_code = models.URLField()
