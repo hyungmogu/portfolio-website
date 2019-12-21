58b6c365a41e3e94ba49bebf6e8111b99863ca27
Hyungmo Gu <guhyungm7@gmail.com>
2019-12-21 06:08:37 -0800

    feat: Add progress on converting static to dynamic template 'layout'   - add url and template for contact

--- project/main/urls.py
+++ project/main/urls.py
@@ -5,4 +5,5 @@ urlpatterns = [
     path('works/<int:pk>', views.WorksDetailView.as_view(), name='works_detail'),
     path('works', views.WorksView.as_view(), name='works'),
     path('about', views.AboutView.as_view(), name='about'),
+    path('contact', views.AboutView.as_view(), name='contact'),
 ]
