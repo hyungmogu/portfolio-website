55d74dadd643f7557f2ba1296d58d8d15642be51
Hyungmo Gu <guhyungm7@gmail.com>
2019-12-22 22:57:08 -0800

    feat: Add progress on refining website   - direct to 'blog.hyungmogu.com' on clicking 'blogs' in navigation menu

--- project/main/urls.py
+++ project/main/urls.py
@@ -1,10 +1,14 @@
 import main.views as views
 from django.urls import path
+from django.views.generic.base import RedirectView
+
+domain_name = 'hyungmogu.com'
 
 urlpatterns = [
     path('works/<int:pk>', views.WorksDetailView.as_view(), name='works_detail'),
     path('works', views.WorksView.as_view(), name='works'),
     path('about', views.AboutView.as_view(), name='about'),
     path('contact', views.ContactView.as_view(), name='contact'),
+    path('blogs', RedirectView.as_view(url='http://blog.{}'.format(domain_name)), name='blogs'),
     path('', views.HomeView.as_view(), name='home'),
 ]
